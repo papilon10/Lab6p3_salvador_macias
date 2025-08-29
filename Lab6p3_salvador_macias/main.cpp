82e32465e294708e6a9995b7163ef15c052d3439
papilon10 <salvigna17@gmail.com>
2025-08-29 13:27:09 -0600

    inicio de proyecto

--- Lab6p3_salvador_macias/main.cpp
+++ Lab6p3_salvador_macias/main.cpp
@@ -1,14 +1,11 @@
-//
-//  main.cpp
-//  Lab6p3_salvador_macias
-//
-//  Created by salvador macias on 29/08/2025.
-//
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
+   cout << "Hello, World!\n";
     return 0;
 }
